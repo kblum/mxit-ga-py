0c980108c0d94fc807545284c0945b42ddaa1b5d
Konrad Blum <konrad.blum@gmail.com>
2012-10-22 21:18:41 +0200

    Update installation script.

--- setup.py
+++ setup.py
@@ -13,16 +13,19 @@ setup(
     name = "mxit_ga",
     version = "0.1.0",
     author = "Konrad Blum",
-    author_email = "konrad.blum@gmail.com",
+    author_email = "konrad@kblum.com",
     description = "Python module to perform Google Analytics tracking for Mxit web applications.",
     license = "TODO",
     keywords = "google analytics mxit",
     url = "https://github.com/kblum/mxit-ga-py",
+    download_url = "https://github.com/kblum/mxit-ga-py/downloads",
     packages = ["mxit_ga",],
     long_description = read("README.md"),
     classifiers = [
         "Development Status :: 4 - Beta",
         "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ]
 )
